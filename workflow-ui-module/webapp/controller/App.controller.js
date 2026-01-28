fdaaf3d2f7690dd3d3c8b84fd8ca8484be2472d3
Gunalan X <gunalan.x.extern@lapp.com>
2026-01-28 14:08:49 +0000

    Removed commented code

--- workflow-ui-module/webapp/controller/App.controller.js
+++ workflow-ui-module/webapp/controller/App.controller.js
@@ -123,27 +123,7 @@ sap.ui.define(
         oContextModel.setProperty("/componentVisible", false);
       },
 
-      // onSearch: function (oEvent) {
-      //   var sValue = oEvent.getParameter("newValue");
-      //   var oTable = this.byId("obsoleteTable");
-      //   var oBinding = oTable.getBinding("items");
 
-      //   var aFilters = [];
-      //   if (sValue) {
-      //     aFilters.push(
-      //       new sap.ui.model.Filter({
-      //         filters: [
-      //           new sap.ui.model.Filter("rfqId", sap.ui.model.FilterOperator.Contains, sValue),
-      //           new sap.ui.model.Filter("customer", sap.ui.model.FilterOperator.Contains, sValue),
-      //           new sap.ui.model.Filter("caused", sap.ui.model.FilterOperator.Contains, sValue)
-      //         ],
-      //         and: false
-      //       })
-      //     );
-      //   }
-
-      //   oBinding.filter(aFilters);
-      // },
 
       onSearch: function (oEvent) {
         var sValue = oEvent.getParameter("newValue");
@@ -205,38 +185,6 @@ sap.ui.define(
         oTable.clearSelection();
       },
 
-      // onSortConfirm: function (oEvent) {
-      //   var sKey = oEvent.getParameter("sortItem").getKey();
-      //   var bDesc = oEvent.getParameter("sortDescending");
-
-      //   var oTable = this.byId("obsoleteTable");
-      //   var oBinding = oTable.getBinding("items");
-
-      //   var oSorter = new sap.ui.model.Sorter(sKey, bDesc);
-      //   oBinding.sort(oSorter);
-      // },
-
-
-      // onUpdateHandling: function () {
-      //   var oTable = this.byId("obsoleteTable");
-      //   this._aSelectedItems = oTable.getSelectedItems();
-
-      //   if (!this._aSelectedItems.length) {
-      //     sap.m.MessageToast.show("Please select at least one row");
-      //     return;
-      //   }
-
-      //   if (!this._oHandlingDialog) {
-      //     this._oHandlingDialog = sap.ui.xmlfragment(
-      //       "obsoletetaskform.workflowuimodule.view.fragment.HandlingUpdate",
-      //       this
-      //     );
-      //     this.getView().addDependent(this._oHandlingDialog);
-      //   }
-
-      //   sap.ui.getCore().byId("massHandlingSelect").setSelectedKey("");
-      //   this._oHandlingDialog.open();
-      // },
 
       onUpdateHandling: function () {
         var oTable = this.byId("obsoleteTable");
@@ -356,63 +304,7 @@ sap.ui.define(
         sap.m.MessageToast.show("Scrapping updated successfully");
       },
 
-      // onApplyHandlingUpdate: function () {
-      //   var sSelectedHandling = sap.ui.getCore()
-      //     .byId("massHandlingSelect")
-      //     .getSelectedKey();
 
-      //   if (!sSelectedHandling) {
-      //     sap.m.MessageToast.show("Please select Handling value");
-      //     return;
-      //   }
-
-      //   this._aSelectedItems.forEach(function (oItem) {
-      //     var oCtx = oItem.getBindingContext("context");
-      //     oCtx.getObject().handling = sSelectedHandling;
-      //   });
-
-      //   this.getView().getModel("context").refresh(true);
-
-      //   this._oHandlingDialog.close();
-
-      //   sap.m.MessageToast.show("Handling updated successfully");
-      // },
-      // onOpenFilterDialog: function () {
-      //   if (!this._oFilterDialog) {
-      //     this._oFilterDialog = new sap.m.ViewSettingsDialog({
-      //       title: "Filter",
-      //       filterItems: [
-      //         new sap.m.ViewSettingsFilterItem({
-      //           text: "Plant",
-      //           key: "plant",
-      //           items: [
-      //             new sap.m.ViewSettingsItem({ text: "1000", key: "1000" }),
-      //             new sap.m.ViewSettingsItem({ text: "2000", key: "2000" })
-      //           ]
-      //         }),
-      //         new sap.m.ViewSettingsFilterItem({
-      //           text: "Customer",
-      //           key: "customer",
-      //           items: [
-      //             new sap.m.ViewSettingsItem({ text: "TATA", key: "TATA" }),
-      //             new sap.m.ViewSettingsItem({ text: "BMW", key: "BMW" })
-      //           ]
-      //         }),
-      //         new sap.m.ViewSettingsFilterItem({
-      //           text: "Handling",
-      //           key: "handling",
-      //           items: [
-      //             new sap.m.ViewSettingsItem({ text: "Scrapping", key: "Scrapping" }),
-      //             new sap.m.ViewSettingsItem({ text: "Alternative use", key: "Alternative use" })
-      //           ]
-      //         })
-      //       ],
-      //       confirm: this.onFilterConfirm.bind(this)
-      //     });
-      //   }
-
-      //   this._oFilterDialog.open();
-      // },
       onOpenFilterDialog: function () {
         const oModel = this.getView().getModel("context");
         const aItems = oModel.getProperty("/obsoleteItems");
@@ -541,25 +433,7 @@ sap.ui.define(
         oTable.clearSelection();
       },
 
-      // onFilterConfirm: function (oEvent) {
-      //   var aSelectedFilterItems = oEvent.getParameter("filterItems"); // 
-      //   var aFilters = [];
 
-      //   aSelectedFilterItems.forEach(function (oItem) {
-      //     aFilters.push(
-      //       new sap.ui.model.Filter(
-      //         oItem.getParent().getKey(),                    // plant / customer
-      //         sap.ui.model.FilterOperator.EQ,
-      //         oItem.getText()                      // selected value
-      //       )
-      //     );
-      //   });
-
-      //   var oTable = this.byId("obsoleteTable");
-      //   var oBinding = oTable.getBinding("items");
-
-      //   oBinding.filter(aFilters);
-      // },
       onDeleteSelected: function () {
         var oTable = this.byId("obsoleteTable");
         var oModel = this.getView().getModel("context");
@@ -598,104 +472,7 @@ sap.ui.define(
       },
 
 
-      // onFilterConfirm: function (oEvent) {
-      //   var aSelectedFilters = oEvent.getParameter("filterItems");
-      //   var aFilters = [];
 
-      //   aSelectedFilters.forEach(function (oItem) {
-      //     aFilters.push(
-      //       new sap.ui.model.Filter(
-      //         oItem.getKey(),
-      //         sap.ui.model.FilterOperator.EQ,
-      //         oItem.getText()
-      //       )
-      //     );
-      //   });
-
-      //   var oTable = this.byId("obsoleteTable");
-      //   oTable.getBinding("items").filter(aFilters);
-      // },
-      // onDeleteSelected: function () {
-      //   var oTable = this.byId("obsoleteTable");
-      //   var oModel = this.getView().getModel("context");
-
-      //   // Get full list of items
-      //   var aItems = oModel.getProperty("/obsoleteItems") || [];
-
-      //   // Get selected items
-      //   var aSelectedContexts = oTable.getSelectedContexts();
-
-      //   if (aSelectedContexts.length === 0) {
-      //     sap.m.MessageToast.show("Please select at least one row to delete.");
-      //     return;
-      //   }
-
-      //   // Collect indices of selected rows
-      //   var aIndicesToDelete = aSelectedContexts.map(ctx => {
-      //     return parseInt(ctx.getPath().split("/").pop());
-      //   });
-
-      //   // Remove selected rows (sorted descending to avoid index shift)
-      //   aIndicesToDelete.sort((a, b) => b - a).forEach(index => {
-      //     aItems.splice(index, 1);
-      //   });
-
-      //   // Update model
-      //   oModel.setProperty("/obsoleteItems", aItems);
-
-      //   // Clear selection
-      //   oTable.removeSelections();
-
-      //   sap.m.MessageToast.show("Selected rows deleted.");
-      // },
-      // onUpdateComments: function () {
-      //   var oTable = this.byId("obsoleteTable");
-      //   var aSelectedContexts = oTable.getSelectedContexts();
-
-      //   if (aSelectedContexts.length === 0) {
-      //     sap.m.MessageToast.show("Please select at least one row.");
-      //     return;
-      //   }
-
-      //   if (!this._oUpdateCommentsDialog) {
-      //     this._oUpdateCommentsDialog = sap.ui.xmlfragment(
-      //       "obsoletetaskform.workflowuimodule.view.fragment.UpdateCommentsDialog",
-      //       this
-      //     );
-      //     this.getView().addDependent(this._oUpdateCommentsDialog);
-      //   }
-
-      //   // Clear previous value
-      //   sap.ui.getCore().byId("bulkComments").setValue("");
-
-      //   this._oUpdateCommentsDialog.open();
-      // },
-      // onApplyComments: function () {
-      //   var oTable = this.byId("obsoleteTable");
-      //   var oModel = this.getView().getModel("context");
-      //   var sComment = sap.ui.getCore().byId("bulkComments").getValue();
-
-      //   if (!sComment) {
-      //     sap.m.MessageToast.show("Please enter a comment.");
-      //     return;
-      //   }
-
-      //   var aItems = oModel.getProperty("/obsoleteItems");
-      //   var aSelectedContexts = oTable.getSelectedContexts();
-
-      //   aSelectedContexts.forEach(function (oContext) {
-      //     var iIndex = oContext.getPath().split("/").pop();
-      //     aItems[iIndex].comments = sComment;
-      //   });
-
-      //   oModel.setProperty("/obsoleteItems", aItems);
-      //   oTable.removeSelections();
-
-      //   this._oUpdateCommentsDialog.close();
-      //   // this._oUpdateCommentsDialog.destroy();
-
-      //   sap.m.MessageToast.show("Comments updated for selected rows.");
-      // },
 
       onUpdateComments: function () {
         var oTable = this.byId("obsoleteTable");
@@ -735,7 +512,7 @@ sap.ui.define(
           return;
         }
 
-        // ✅ update model via binding contexts
+        //  update model via binding contexts
         this._aSelectedContexts.forEach(function (oContext) {
           oContext.getModel().setProperty(
             oContext.getPath() + "/comments",
